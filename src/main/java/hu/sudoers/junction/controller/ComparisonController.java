4dc50519ff4fc70dbc12e32c65284cbbfc3c6d99
tamas.hiter <tamas.hiter@antavo.com>
2021-10-22 20:10:53 +0200

    Comparison endpoint

---
+++ src/main/java/hu/sudoers/junction/controller/ComparisonController.java
@@ -0,0 +1,23 @@
+package hu.sudoers.junction.controller;
+
+import hu.sudoers.junction.service.ComparisonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comparison")
+@RequiredArgsConstructor
+public class ComparisonController {
+
+    private final ComparisonService comparisonService;
+
+    @GetMapping("/disclaimer")
+    public ResponseEntity<String> createQuote(
+        @RequestParam String sourceCurrency,
+        @RequestParam String targetCurrency,
+        @RequestParam String sendAmount
+    ) {
+        return ResponseEntity.ok(comparisonService.createDisclaimer(sourceCurrency, targetCurrency, sendAmount));
+    }
+}
