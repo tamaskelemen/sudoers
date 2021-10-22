fcb1fce37e738bc03b48a69031d749e49cb05171
ricsibata <ricsibata@hotmail.com>
2021-10-22 22:09:22 +0200

    Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import '@transferwise/neptune-css/dist/css/neptune.css';
+import '@transferwise/icons/lib/styles/main.min.css';
+import '@transferwise/components/build/main.css';
+
 import './App.css';
+import Navigation from './components/navigation/Navigation';
+import {Provider} from '@transferwise/components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider i18n={{ locale: 'en', messages: {} }}>
+      <Navigation>
+        Hello World
+      </Navigation>
+    </Provider>
   );
 }
 
