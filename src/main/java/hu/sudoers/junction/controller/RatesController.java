5cb334017f55c06a779a8902efe026daf837c668
kelementamas <tamas.kelemen@antavo.com>
2021-10-22 21:26:06 +0200

    elso vegpont. Nehogy mar olyan javazzon aki nem tud fozni!

--- src/main/java/hu/sudoers/junction/controller/RatesController.java
+++ src/main/java/hu/sudoers/junction/controller/RatesController.java
@@ -1,16 +1,39 @@
 package hu.sudoers.junction.controller;
 
+import hu.sudoers.junction.entity.RateEntity;
+import hu.sudoers.junction.repository.RateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/api")
 public class RatesController {
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private RateRepository repository;
     @RequestMapping(value = "/rates", produces = "application/json")
     @ResponseBody
-    public String list(@RequestParam(name = "source") String source, @RequestParam(name = "target") String target) {
+    public List<RateEntity> list(
+            @RequestParam(name = "source") String source,
+            @RequestParam(name = "target") String target,
+            @RequestParam(name = "from") String from,
+            @RequestParam(name = "to") String to
+            ) {
 
-        return "Source: " + source + ", target: " + target;
+        Query query = new Query();
+        query.addCriteria(Criteria.where("source").is(source));
+        query.addCriteria(Criteria.where("target").is(target));
+        query.addCriteria(Criteria.where("date").gte(from).lte(to));
+
+        return mongoTemplate.find(query, RateEntity.class);
     }
 
 }
