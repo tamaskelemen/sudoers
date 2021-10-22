833cbae4bcf9a3d2ffd1af9c10fe2f15e4245f4c
Nandor.Vigh <nandor.vigh@4ig.hu>
2021-10-22 11:46:44 +0200

    project init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM adoptopenjdk/openjdk11:jdk-11.0.9_11.1-alpine-slim AS base
+FROM base as builder
+
+WORKDIR /project
+
+COPY .mvn /project/.mvn
+COPY mvnw /project/
+COPY pom.xml /project/pom.xml
+
+COPY src /project/src
+
+RUN ./mvnw package -DskipTests
+
+FROM base as app
+
+WORKDIR /app
+EXPOSE 8080
+
+COPY --from=builder project/target/junction-0.0.1-SNAPSHOT.jar  /app/junction-0.0.1-SNAPSHOT.jar
+CMD ["/bin/sh", "-c", "java $JAVA_OPTS -jar junction-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/hu/sudoers/junction/controller/Sample.java
@@ -0,0 +1,16 @@
+package hu.sudoers.junction.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class Sample {
+
+    @GetMapping("ping")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("ping");
+    }
+}
