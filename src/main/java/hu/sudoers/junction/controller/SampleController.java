37d4d282c10f4d0deacf2480394ca9039e9a8cac
kelementamas <tamas.kelemen@antavo.com>
2021-10-22 16:54:33 +0200

    some refact

--- src/main/java/hu/sudoers/junction/controller/SampleController.java
+++ src/main/java/hu/sudoers/junction/controller/SampleController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
-public class Sample {
+public class SampleController {
 
     @Autowired
     private SampleRepository repository;
--- src/main/java/hu/sudoers/junction/repository/SampleRepository.java
+++ src/main/java/hu/sudoers/junction/repository/SampleRepository.java
@@ -8,5 +8,7 @@ import java.util.UUID;
 
 @Repository
 public interface SampleRepository extends MongoRepository<SampleEntity, UUID> {
-
+//    public SampleEntity getLast() {
+//
+//    }
 }
