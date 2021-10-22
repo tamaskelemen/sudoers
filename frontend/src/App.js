1397d755b7408628c02a354aaf68dd37ca767480
ricsibata <ricsibata@hotmail.com>
2021-10-22 22:43:20 +0200

    Container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+import '@transferwise/neptune-css/dist/css/neptune.css';
+import '@transferwise/icons/lib/styles/main.min.css';
+import '@transferwise/components/build/main.css';
+
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import {Provider} from '@transferwise/components';
