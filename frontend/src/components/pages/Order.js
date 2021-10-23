8dcf35f2bd1dc13152d80bec3ee1fc8a0cf45ed5
Nandor.Vigh <nandor.vigh@4ig.hu>
2021-10-24 00:50:14 +0200

    csúnyák a reactes táblázatok

--- frontend/src/components/pages/Order.js
+++ frontend/src/components/pages/Order.js
@@ -1,68 +1,19 @@
-import {Component} from "react";
-import {Space, Table} from "antd";
+import {Component, forwardRef} from "react";
 import {orderList} from "../../client/api";
 import {DeleteTwoTone, EditTwoTone, PauseCircleOutlined, PlayCircleOutlined} from "@ant-design/icons";
+import {
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow
+} from "@mui/material";
 
 class Order extends Component {
-
-    columns = [
-        {
-            title: 'From',
-            dataIndex: 'from',
-            key: 'from',
-            align: 'center',
-        },
-        {
-            title: 'To',
-            dataIndex: 'to',
-            key: 'to',
-            align: 'center',
-        },
-        {
-            title: 'Amount',
-            dataIndex: 'amount',
-            key: 'amount',
-            align: 'center',
-        },
-        {
-            title: 'Threshold',
-            dataIndex: 'threshold',
-            key: 'threshold',
-            align: 'center',
-        },
-        {
-            title: 'Status',
-            dataIndex: 'status',
-            key: 'status',
-            align: 'center',
-        },
-        {
-            title: 'Source',
-            dataIndex: 'source',
-            key: 'source',
-            align: 'center',
-        },
-        {
-            title: 'Target',
-            dataIndex: 'target',
-            key: 'target',
-            align: 'center',
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            align: 'center',
-            render: (text, record) => (
-                <Space size="middle">
-                    <DeleteTwoTone style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff"}}/>
-                    <EditTwoTone style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff"}}/>
-                    {record.status == 'active' ? <PauseCircleOutlined
-                        style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff"}}/>
-                        : <PlayCircleOutlined style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff"}}/> }
-                </Space>
-            ),
-        },
-    ];
 
     constructor(props) {
         super(props);
@@ -73,18 +24,76 @@ class Order extends Component {
 
     componentDidMount() {
         orderList().then(response => {
-                this.setState({
-                    dataSource: response.data
-                })
-            });
+            this.setState({
+                dataSource: response.data
+            })
+        });
     }
 
     render() {
         let data = this.state.dataSource;
+        console.log(data);
         return (
-            <div>
-                <Table dataSource={data} columns={this.columns} />
-            </div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table" pageSize={5}
+                       rowsPerPageOptions={[5]}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">From</TableCell>
+                            <TableCell align="center">To</TableCell>
+                            <TableCell align="center">Amount</TableCell>
+                            <TableCell align="center">Threshold</TableCell>
+                            <TableCell align="center">Status</TableCell>
+                            <TableCell align="center">Source</TableCell>
+                            <TableCell align="center">Target</TableCell>
+                            <TableCell align="center">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell align="center">{row.from}</TableCell>
+                                <TableCell align="center">{row.to}</TableCell>
+                                <TableCell align="center">{row.amount}</TableCell>
+                                <TableCell align="center">{row.threshold}</TableCell>
+                                <TableCell align="center">{row.status}</TableCell>
+                                <TableCell align="center">{row.source}</TableCell>
+                                <TableCell align="center">{row.target}</TableCell>
+                                <TableCell align="center">
+                                    <DeleteTwoTone
+                                        style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff", padding: "10px"}}/>
+                                     <EditTwoTone style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff",
+                                         padding: "10px"}}/>
+                                         {row.status == 'active' ? <PauseCircleOutlined
+                                             style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff", padding: "10px"}}/>
+                                             : <PlayCircleOutlined style={{cursor: "pointer", fontSize: "16px", color: "#00b9ff", padding: "10px"}}/> }
+
+                                    </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                colSpan={3}
+                                count={"113"}
+                                rowsPerPage={"10"}
+                                page={"0"}
+                                SelectProps={{
+                                    inputProps: {
+                                        'aria-label': 'rows per page',
+                                    },
+                                    native: true,
+                                }}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
         );
     }
 }
