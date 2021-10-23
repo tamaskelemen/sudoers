75976403d1dcb151fea0114bc2cbb965e55cd9de
kelementamas <tamas.kelemen@antavo.com>
2021-10-23 03:31:10 +0200

    kiégek

--- src/main/java/hu/sudoers/junction/JunctionApplication.java
+++ src/main/java/hu/sudoers/junction/JunctionApplication.java
@@ -11,14 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMongoRepositories()
 public class JunctionApplication {
 
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				System.out.println("gec");
-				registry.addMapping("/*").allowedOrigins("*");
-			}
-		};
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(JunctionApplication.class, args);
 	}
