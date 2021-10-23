a3446b7336a3bd3a6adb5b9b8586e23c92d02da4
kelementamas <tamas.kelemen@antavo.com>
2021-10-23 09:46:19 +0200

    rename commision to order

--- frontend/src/client/api.js
+++ frontend/src/client/api.js
@@ -1,9 +1,13 @@
 const axios = require('axios');
 const querystring = require('querystring');
-var baseUrl = "http://localhost:8080/api/rates?";
+const repository = require("./repository")
+var baseUrl = "http://localhost:8080";
 
 
-const chart = function (from, to, source, target) {
+/**
+ * Return the chart data as array.
+*/
+const chart =  (from, to, source, target) => {
 
     var params = {
         "from": from,
@@ -15,7 +19,7 @@ const chart = function (from, to, source, target) {
     let queryString = querystring.stringify(params);
     console.log(queryString);
 
-    const response = axios.get(baseUrl + queryString)
+    const response = axios.get(baseUrl + "/api/rates?" + queryString)
         .then(function (response) {
             // handle success
             console.log(response);
@@ -27,6 +31,56 @@ const chart = function (from, to, source, target) {
             return [];
           });
 
+};
+
+const setOrder = function(from, to, source, target, threshold, amount) {
+    var params = {
+        "from": from,
+        "to": to,
+        "source": source,
+        "target": target,
+        "amount": amount,
+        "threshold": threshold
+    }
+
+    axios.post(baseUrl + "/api/order/create", params)
+         .then(function (response) {
+               // handle success
+               console.log(response);
+               return response;
+             })
+             .catch(function (error) {
+               // handle error
+               console.log(error);
+               return [];
+         });
+};
+
+//Returns how much you get by using tw
+const disclaimer = function(source, target, amount) {
+    var params = {
+        "sourceCurrency": source,
+        "targetCurrency": target,
+        "sendAmount": amount
+    }
+
+    let queryString = querystring.stringify(params);
+    console.log(queryString);
+    axios.get(baseUrl + "/api/comparison/disclaimer?" + queryString)
+             .then(function (response) {
+                   // handle success
+                   console.log(response);
+                   return response;
+                 })
+                 .catch(function (error) {
+                   // handle error
+                   console.log(error);
+                   return [];
+             });
 }
 
 chart("2017-10-01", "2018-11-01", "AED", "USD");
+setOrder("2017-10-01", "2018-11-01", "AED", "USD", 10, 1555);
+disclaimer("USD", "HUF", 10000);
+
+payments.fund();
--- src/main/java/hu/sudoers/junction/controller/OrderController.java
+++ src/main/java/hu/sudoers/junction/controller/OrderController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/api/order")
@@ -21,6 +18,7 @@ public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping(value="/create", method = RequestMethod.POST)
     public ResponseEntity<String> save(@RequestBody OrderDto orderDto) {
         OrderEntity orderEntity = new OrderEntity(orderDto);
@@ -30,6 +28,7 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping(value="/status", method = RequestMethod.POST)
     @Transactional
     public ResponseEntity<String> status(@RequestBody OrderStatusRequest orderStatusRequest) {
